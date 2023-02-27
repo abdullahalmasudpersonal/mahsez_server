a60c152944a293e359e00bc99bc6438cecd4fee5
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2023-02-28 00:03:00 +0600

    another update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const app = express();
 
